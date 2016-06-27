d4f7688a4a3faafd3a8971976072b17e53959010
superburrito <chuayaohui@gmail.com>
2016-06-27 18:20:19 -0400

    some changes

--- models/database.js
+++ models/database.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost/tripplanner', {
-	logging: true
+	logging: false
 });
 
 module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ var router = express.Router();
 
 var bodyParser = require('body-parser');
 var Sequelize = require('sequelize');
-var db = require('./models/database');
-var Activity = require('./models/activity');
-var Hotel = require('./models/hotel');
-var Place = require('./models/place');
-var Restaurant = require('./models/restaurant');
+var db = require('../models/database');
+var Activity = require('../models/activity');
+var Hotel = require('../models/hotel');
+var Place = require('../models/place');
+var Restaurant = require('../models/restaurant');
 var Promise = require('bluebird');
-
 router.get('/', function(req,res,next){
 	var ActivityProm = Activity.findAll({});
 	var HotelProm = Hotel.findAll({});
@@ -17,14 +16,11 @@ router.get('/', function(req,res,next){
 	var RestaurantProm = Restaurant.findAll({});
 	Promise.all([ActivityProm, HotelProm, PlaceProm, RestaurantProm])
 	.then(function(data){
-		console.log(data);
-		res.render('index', {templateActivity: data[0],
-												 templateHotel: data[1],
-												 templatePlace: data[2],
-												 templateRestaurant: data[3]});
+		res.render('index', {templateActivity: data[0], templateHotel: data[1], templatePlace: data[2], templateRestaurant: data[3]});
 	})
 	.catch(next);
 });
 
 
 
+module.exports = router;
